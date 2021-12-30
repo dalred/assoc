9cb025faa32a1748145836c35f8d666457de624d
dalred <RcrF63gGG7HpbAT>
2021-12-30 07:06:19 +0300

    add join()

--- AssocV.2.1.ps1
+++ AssocV.2.1.ps1
@@ -6,7 +6,7 @@
 мы воспользовались сборкой Microsoft.Win32.Registry, небольшие нарекания есть, но в целом отрабатывает хорошо.
 #>
 
-
+Add-Type -Assembly PresentationCore
 [System.Text.Encoding]::GetEncoding("cp866") | Out-Null
 $HKUpath = "Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts"
 $Logfile = "$env:TEMP\assocscript_$env:computername.log"
@@ -25,73 +25,73 @@ function main
 {
 	param ($program)
 	$hash_object = @{
-		"МойОфис Текст"	      = @{path="$program\MyOffice\MyOffice Text.exe"; name=("TextEditor.docx", "TextEditor.doc", "TextEditor.rtf")};
-		"МойОфис Таблица"	  = @{path="$program\MyOffice\MyOffice Spreadsheet.exe"; name=("SpreadsheetEditor.xls", "SpreadsheetEditor.xlsx", "SpreadsheetEditor.csv", "SpreadsheetEditor.xlsm")};
-		"МойОфис Презентация" = @{path="$program\MyOffice\MyOffice Presentation.exe"; name=("PresentationViewer.pptx", "PresentationViewer.ppt")};
+		"МойОфис Текст" = @{ path = "$program\MyOffice\MyOffice Text.exe"; name = ("TextEditor.docx", "TextEditor.doc", "TextEditor.rtf") };
+		"МойОфис Таблица" = @{ path = "$program\MyOffice\MyOffice Spreadsheet.exe"; name = ("SpreadsheetEditor.xls", "SpreadsheetEditor.xlsx", "SpreadsheetEditor.csv", "SpreadsheetEditor.xlsm") };
+		"МойОфис Презентация" = @{ path = "$program\MyOffice\MyOffice Presentation.exe"; name = ("PresentationViewer.pptx", "PresentationViewer.ppt") };
 	}
 	$MyPSObject = New-Object -TypeName psobject -Property $hash_object
 	$RegistryValueKind = [Microsoft.Win32.RegistryValueKind]::String
-	
 	$MyPSObject.PSObject.Properties | ForEach-Object {
 		$name = $_.Name
 		$_.Value | ForEach-Object {
-        $path=$_.path
-            if (-Not (Test-Path -Path $path))
-    		{
-    			throw ("Не существует пути: {0}" -f $path)
-    		}
-            $_.name| ForEach-Object {
-                $ftype = $_
-                $Ext = $ftype.Split(".")[1]
-                WriteLog -LogString "assoc .$Ext=$ftype"
-                $cmdOutput=cmd /c assoc `".$Ext`"=`""$ftype`"" 2>&1
-                WriteLog -LogString "ftype $ftype=$path"
-                $cmdOutput+=cmd /c ftype `"$ftype`"=`""$path`"" `""%1`"" 2>&1
-                if ($LASTEXITCODE -ne 0) {
-                    throw ($cmdOutput)
-                }
-			    $parent = [Microsoft.Win32.Registry]::ClassesRoot.OpenSubKey($ftype, $true)
-			    $appKey = [Microsoft.Win32.Registry]::ClassesRoot.OpenSubKey("$ftype\Application")
-			    If (-not $appKey)
-			    {
-				    WriteLog -LogString "CreateSubKey Application"
-				    $parent.CreateSubKey("Application") | out-null
-				    $parent_user = $parent.OpenSubKey('Application', $true)
-				    WriteLog -LogString "SetValue ApplicationName $name"
-				    $parent_user.SetValue("ApplicationName", $name, $RegistryValueKind)
-				    $parent_user.Close()
-			    }
-			    $Icon = [Microsoft.Win32.Registry]::ClassesRoot.OpenSubKey("$ftype\DefaultIcon")
-			    If (-not $Icon)
-			    {
-				    WriteLog -LogString "CreateSubKey DefaultIcon"
-				    $parent.CreateSubKey("DefaultIcon") | out-null
-				    $parent_user = $parent.OpenSubKey('DefaultIcon', $true)
-				    $ExtUP = $Ext.ToUpper()
-				    WriteLog -LogString "SetValue to DefaultIcon $program\MyOffice\$ExtUP.ico"
-				    $parent_user.SetValue($null, """$program\MyOffice\$ExtUP.ico""", $RegistryValueKind)
-				    $parent_user.Close()
-				
-			    }
-			    $parent.Close()
-			    $parent = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey("$HKUpath\.$Ext", $true)
-			    $Progid = [Microsoft.Win32.Registry]::GetValue("HKEY_CURRENT_USER\$HKUpath\.$Ext\UserChoice", "Progid", $null)
-			    if ($Progid)
-			    {
-				    WriteLog -LogString "Progid was $Progid"
-				    WriteLog -LogString "DeleteSubKey UserChoice in $ftype type $Ext"
-				    $parent.DeleteSubKey('UserChoice', $true)
-				    WriteLog -LogString "CreateSubKey UserChoice  in $ftype type $Ext"
-				    $parent.CreateSubKey("UserChoice") | out-null
-				    $parent_user = $parent.OpenSubKey('UserChoice', $true)
-				    WriteLog -LogString "SetValue Progid $ftype type $Ext"
-				    $parent_user.SetValue("Progid", $ftype, $RegistryValueKind)
-				    $parent_user.Close()
-			    }
-			    $parent.Close()
-		    }
-	    }
-    }
+			$path = $_.path
+			if (-Not (Test-Path -Path $path))
+			{
+				throw ("Не существует пути: {0}" -f $path)
+			}
+			$_.name | ForEach-Object {
+				$ftype = $_
+				$Ext = $ftype.Split(".")[1]
+				WriteLog -LogString "assoc .$Ext=$ftype"
+				$cmdOutput = [string]::Join(" ", (cmd /c assoc `".$Ext`"=`""$ftype`"" 2>&1)+"`n")
+				WriteLog -LogString "ftype $ftype=$path"
+				$cmdOutput += [string]::Join(" ", (cmd /c ftype `"$ftype`"=`""$path`"" `""%1`"" 2>&1) + "`n")
+				if ($LASTEXITCODE -ne 0)
+				{
+					throw ($cmdOutput)
+				}
+				$parent = [Microsoft.Win32.Registry]::ClassesRoot.OpenSubKey($ftype, $true)
+				$appKey = [Microsoft.Win32.Registry]::ClassesRoot.OpenSubKey("$ftype\Application")
+				If (-not $appKey)
+				{
+					WriteLog -LogString "CreateSubKey Application"
+					$parent.CreateSubKey("Application") | out-null
+					$parent_user = $parent.OpenSubKey('Application', $true)
+					WriteLog -LogString "SetValue ApplicationName $name"
+					$parent_user.SetValue("ApplicationName", $name, $RegistryValueKind)
+					$parent_user.Close()
+				}
+				$Icon = [Microsoft.Win32.Registry]::ClassesRoot.OpenSubKey("$ftype\DefaultIcon")
+				If (-not $Icon)
+				{
+					WriteLog -LogString "CreateSubKey DefaultIcon"
+					$parent.CreateSubKey("DefaultIcon") | out-null
+					$parent_user = $parent.OpenSubKey('DefaultIcon', $true)
+					$ExtUP = $Ext.ToUpper()
+					WriteLog -LogString "SetValue to DefaultIcon $program\MyOffice\$ExtUP.ico"
+					$parent_user.SetValue($null, """$program\MyOffice\$ExtUP.ico""", $RegistryValueKind)
+					$parent_user.Close()
+					
+				}
+				$parent.Close()
+				$parent = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey("$HKUpath\.$Ext", $true)
+				$Progid = [Microsoft.Win32.Registry]::GetValue("HKEY_CURRENT_USER\$HKUpath\.$Ext\UserChoice", "Progid", $null)
+				if ($Progid)
+				{
+					WriteLog -LogString "Progid was $Progid"
+					WriteLog -LogString "DeleteSubKey UserChoice in $ftype type $Ext"
+					$parent.DeleteSubKey('UserChoice', $true)
+					WriteLog -LogString "CreateSubKey UserChoice  in $ftype type $Ext"
+					$parent.CreateSubKey("UserChoice") | out-null
+					$parent_user = $parent.OpenSubKey('UserChoice', $true)
+					WriteLog -LogString "SetValue Progid $ftype type $Ext"
+					$parent_user.SetValue("Progid", $ftype, $RegistryValueKind)
+					$parent_user.Close()
+				}
+				$parent.Close()
+			}
+		}
+	}
 	
 }
 
@@ -108,24 +108,24 @@ if (-Not (Test-Path -Path HKU:\))
 
 try
 {
-    if ([IntPtr]::Size -eq 8)
-    {
-	    main -program $env:ProgramFiles
-
-    }
-    else
-    {
+	if ([IntPtr]::Size -eq 8)
+	{
+		main -program $env:ProgramFiles
+		
+	}
+	else
+	{
 		main -program ${env:ProgramFiles(x86)}
-	
-    }
+		
+	}
 }
 catch
-	{
-		$ErrorMessage = $_.Exception.Message
-		$FailedItem = $_.Exception.ItemName
-		Write-warning ("Ошибка! {0} - {1}" -f $ErrorMessage, $FailedItem)
-		WriteLog -LogString ("Ошибка! {0} - {1}" -f $ErrorMessage, $FailedItem)
-	}
+{
+	$ErrorMessage = $_.Exception.Message
+	$FailedItem = $_.Exception.ItemName
+	Write-warning ("Ошибка! {0} - {1}" -f $ErrorMessage, $FailedItem)
+	WriteLog -LogString ("Ошибка! {0} - {1}" -f $ErrorMessage, $FailedItem)
+}
 finally
 {
 	WriteLog -LogString "Finish work!"
